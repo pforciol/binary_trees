7a430a7d55b42d1a077c37f8b7633caf7347bbe6
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-08 10:37:48 +0000

    Add 10th task, update header file

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree)
+	{
+		while (tree->parent)
+		{
+			depth++;
+			tree = tree->parent;
+		}
+	}
+
+	return (depth);
+}
