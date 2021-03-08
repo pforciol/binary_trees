83546aa62bfc0f58aaab5736393e7a3c06ee64d0
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-08 12:30:26 +0000

    Add 14th task, update header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -29,11 +31,14 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
