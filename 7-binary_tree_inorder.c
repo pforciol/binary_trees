2c2e276bb3b7c809b16255b7937e5f48c95f35f7
Tarek <kheirtarek@hotmail.fr>
2021-03-09 14:12:59 +0100

    task 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree->left)
+        binary_tree_inorder(tree->left, func);
+    func(tree->n);
+    if (tree->right)
+        binary_tree_inorder(tree->right, func);
+}
