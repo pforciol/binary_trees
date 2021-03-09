21b5a32c6d3264579113a1f99700278286649afe
Tarek <kheirtarek@hotmail.fr>
2021-03-09 14:19:43 +0100

    fix 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,9 +2,12 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree->left)
-        binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    if (tree->right)
-        binary_tree_inorder(tree->right, func);
+    if (tree && func)
+    {
+        if (tree->left)
+            binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        if (tree->right)
+            binary_tree_inorder(tree->right, func);
+    }
 }
