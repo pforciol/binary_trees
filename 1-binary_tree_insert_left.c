3002e051f12f2f024579d6d58ae6512341a566d3
Tarek <kheirtarek@hotmail.fr>
2021-03-09 12:38:52 +0100

    task 1 tarek

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+	binary_tree_t *tmp_node = NULL;
+
+	if (parent)
+	{
+		node = binary_tree_node(parent, value);
+		if (node)
+		{
+			if (parent->left)
+				tmp_node = parent->left;
+			parent->left = node;
+			if (tmp_node)
+				node->left = tmp_node;
+		}
+	}
+
+	return (node);
+}
