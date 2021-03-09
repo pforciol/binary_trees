fae8c05b114ff2f7ed7841d3beeaaed6dd1b583f
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-03-09 11:57:21 +0000

    Add 100th task, update header file

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second);
 
 
 #endif /* BINARY_TREES_H */
