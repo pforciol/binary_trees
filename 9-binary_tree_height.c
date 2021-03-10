725fd635509afb7b206ab5799af5d99da6cba1fa
Tarek <kheirtarek@hotmail.fr>
2021-03-10 13:50:45 +0100

    fix task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,17 +7,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t size = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		size++;
-	if (tree->right)
-		size++;
-	binary_tree_height(tree->left);
-	binary_tree_height(tree->right);
-
-	return (size);
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+		return (binary_tree_height(tree->left) + 1);
+	
+	else
+		return (binary_tree_height(tree->right) + 1);
 }
