0f41e41c16fa02a5e6bd5be777fde20a5ddabb35
Tarek <kheirtarek@hotmail.fr>
2021-03-10 14:00:23 +0100

    fix task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,14 +7,20 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int r1 = 0;
+	int r2 = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right && tree->parent)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
+	r1 = binary_tree_is_full(tree->left);
+	r2 = binary_tree_is_full(tree->right);
+
+	if (r1 && r2)
+		return (1);
 
 	return (0);
 }
